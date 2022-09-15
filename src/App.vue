1a28214fedca73d251d4fd69e8862421abcd952c
grigoriis <grigoriis@kmslh.com>
2022-09-15 10:25:59 +0300

    Change route title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/movies">Movie</router-link>
+    <router-link to="/movies">Movies</router-link>
   </nav>
   <router-view/>
 </template>
